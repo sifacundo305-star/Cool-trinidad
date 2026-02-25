3da3a321f267e2235b132fae49a96c80b28bf1ec
sifacundo305-star <sifacundo305@gmail.com>
2026-02-24 23:40:22 -0300

    Change Supabase key to a new JWT
    Updated Supabase key to a new value.

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = supabase;
 
 const SUPABASE_URL = "https://fwujxwmjckrbqnlioifp.supabase.co";
-const SUPABASE_KEY = "TU_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3dWp4d21qY2tyYnFubGlvaWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4ODQ5OTAsImV4cCI6MjA4NzQ2MDk5MH0.FlPMctJpmOEvBYKBrFEzKgDVjJTHex5K-PFbLOoo1kw";
 
 const client = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
