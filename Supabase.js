5d99606521071c96311ba824769c87d5002e576c
sifacundo305-star <sifacundo305@gmail.com>
2026-02-24 22:48:42 -0300

    Update Supabase URL and key in Supabase.js

--- Supabase.js
+++ Supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = supabase;
 
-const SUPABASE_URL = "TU_SUPABASE_URL";
-const SUPABASE_KEY = "TU_PUBLIC_ANON_KEY";
+const SUPABASE_URL = "https://fwujxwmjckrbqnlioifp.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3dWp4d21qY2tyYnFubGlvaWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4ODQ5OTAsImV4cCI6MjA4NzQ2MDk5MH0.FlPMctJpmOEvBYKBrFEzKgDVjJTHex5K-PFbLOoo1kw";
 
 window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
